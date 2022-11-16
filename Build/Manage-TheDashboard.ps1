54d19d840862ae72463bc8a6844639efcf3595b6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-16 09:25:11 +0100

    Update module builder

--- Build/Manage-TheDashboard.ps1
+++ Build/Manage-TheDashboard.ps1
@@ -8,7 +8,7 @@ $Configuration = @{
 
         Manifest          = @{
             # Version number of this module.
-            ModuleVersion              = '0.0.7'
+            ModuleVersion              = '0.0.X'
             # Supported PSEditions
             CompatiblePSEditions       = @('Desktop', 'Core')
             # ID used to uniquely identify this module
