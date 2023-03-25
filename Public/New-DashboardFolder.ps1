a97c6c993758d083b538e78a030d9e08b897b600
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-25 08:44:12 +0100

    Add builder for Folders

---
+++ Public/New-DashboardFolder.ps1
@@ -0,0 +1,104 @@
+﻿function New-DashboardFolder {
+    [alias('New-TheDashboardFolder')]
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0)][scriptblock] $Replacements,
+        [Parameter(Mandatory)][string] $Name,
+        [Parameter(Mandatory)][string] $Path,
+        [Parameter(Mandatory)][string] $UrlName,
+        [string] $CopyFrom,
+        [string] $MoveFrom,
+        [switch] $DisableGlobalReplacement,
+        # ICON BRANDS
+        [ArgumentCompleter(
+            {
+                param($CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters)
+            ($Global:HTMLIcons.FontAwesomeBrands.Keys)
+            }
+        )]
+        [ValidateScript(
+            {
+                $_ -in (($Global:HTMLIcons.FontAwesomeBrands.Keys))
+            }
+        )]
+        [parameter(ParameterSetName = "FontAwesomeBrands")][string] $IconBrands,
+
+        # ICON REGULAR
+        [ArgumentCompleter(
+            {
+                param($CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters)
+            ($Global:HTMLIcons.FontAwesomeRegular.Keys)
+            }
+        )]
+        [ValidateScript(
+            {
+                $_ -in (($Global:HTMLIcons.FontAwesomeRegular.Keys))
+            }
+        )]
+        [parameter(ParameterSetName = "FontAwesomeRegular")][string] $IconRegular,
+
+        # ICON SOLID
+        [ArgumentCompleter(
+            {
+                param($CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters)
+            ($Global:HTMLIcons.FontAwesomeSolid.Keys)
+            }
+        )]
+        [ValidateScript(
+            {
+                $_ -in (($Global:HTMLIcons.FontAwesomeSolid.Keys))
+            }
+        )]
+        [parameter(ParameterSetName = "FontAwesomeSolid")][string] $IconSolid
+    )
+
+
+    if ($IconBrands) {
+        $IconType = 'IconBrands'
+        $Icon = $IconBrands
+    } elseif ($IconRegular) {
+        $IconType = 'IconRegular'
+        $Icon = $IconRegular
+    } elseif ($IconSolid) {
+        $IconType = 'IconSolid'
+        $Icon = $IconSolid
+    } else {
+        $IconType = 'IconSolid'
+        $Icon = 'folder'
+    }
+
+    if ($Replacements) {
+        $ReplacementConfiguration = [System.Collections.Generic.List[System.Collections.IDictionary]]::new()
+        $OutputElements = & $Replacements
+        foreach ($E in $OutputElements) {
+            # if ($E.Type -eq 'Gage') {
+            #     $GageConfiguration.Add($E.Settings)
+            # } elseif ($E.Type -eq 'Folder') {
+            #     $FoldersConfiguration.Add($E.Settings)
+            if ($E.Type -eq 'Replacement') {
+                $ReplacementConfiguration.Add($E.Settings)
+            }
+        }
+        $ReplacementsConfiguration = Convert-MultipleReplacements -ReplacementConfiguration $ReplacementConfiguration
+
+    } else {
+        $ReplacementsConfiguration = $null
+    }
+
+    $Folder = [ordered] @{
+        Type     = 'Folder'
+        Settings = [ordered] @{
+            Name               = $Name
+            IconType           = $IconType
+            Icon               = $Icon
+            Path               = $Path
+            Url                = $UrlName
+            CopyFrom           = $CopyFrom
+            MoveFrom           = $MoveFrom
+            ReplacementsGlobal = -not $DisableGlobalReplacement.IsPresent
+            Replacements       = $ReplacementsConfiguration
+        }
+    }
+    Remove-EmptyValue -Hashtable $Folder
+    $Folder
+}
