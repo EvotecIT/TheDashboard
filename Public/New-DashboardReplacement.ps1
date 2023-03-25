037e6e237dab6c4e8f1713eca3d53e6e9c45d238
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-25 08:44:54 +0100

    Add builder for replacements

---
+++ Public/New-DashboardReplacement.ps1
@@ -0,0 +1,34 @@
+﻿function New-DashboardReplacement {
+    [alias('New-TheDashboardReplacement')]
+    [CmdletBinding()]
+    param(
+        [string] $SplitOn,
+        [Array] $BeforeSplit,
+        [Array] $AfterSplit,
+        [switch] $AddSpaceToName
+    )
+
+    $BeforeEntry = [ordered] @{}
+    foreach ($Before in $BeforeSplit) {
+        foreach ($Key in $Before.Keys) {
+            $BeforeEntry[$Key] = $Before[$Key]
+        }
+    }
+    $AfterEntry = [ordered] @{}
+    foreach ($After in $AfterSplit) {
+        foreach ($Key in $After.Keys) {
+            $AfterEntry[$Key] = $After[$Key]
+        }
+    }
+
+    $Replacements = [ordered] @{
+        Type     = 'Replacement'
+        Settings = @{
+            SplitOn        = if ($PSBoundParameters.ContainsKey('SplitOn')) { $SplitOn } else { $null }
+            BeforeSplit    = if ($PSBoundParameters.ContainsKey('BeforeSplit')) { $BeforeEntry } else { $null }
+            AfterSplit     = if ($PSBoundParameters.ContainsKey('AfterSplit')) { $AfterEntry } else { $null }
+            AddSpaceToName = if ($PSBoundParameters.ContainsKey('AddSpaceToName')) { $AddSpaceToName } else { $null }
+        }
+    }
+    $Replacements
+}
