8b486d000796d4bd7adc34ef3caa5db806d81922
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-25 08:47:59 +0100

    Translate folders into new configuration

---
+++ Private/Set-FolderConfiguration.ps1
@@ -0,0 +1,10 @@
+﻿function Set-FolderConfiguration {
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary]$Folders,
+        [Array] $FoldersConfiguration
+    )
+    foreach ($Folder in $FoldersConfiguration) {
+        $Folders[$Folder.Name] = $Folder
+    }
+}
