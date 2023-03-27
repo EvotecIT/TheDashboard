6cc39b4016e253490210520d30df31ee55600294
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-27 11:02:09 +0200

    Add fix in case user forgets configuration

--- Private/Set-FolderConfiguration.ps1
+++ Private/Set-FolderConfiguration.ps1
@@ -5,6 +5,11 @@
         [Array] $FoldersConfiguration
     )
     foreach ($Folder in $FoldersConfiguration) {
+        if (-not $Folder.Name -and $Folder.UrlName) {
+            $Folder.Name = $Folder.UrlName
+        } elseif (-not $Folder.UrlName -and $Folder.Name) {
+            $Folder.Url = $Folder.Name
+        }
         $Folders[$Folder.Name] = $Folder
     }
 }
