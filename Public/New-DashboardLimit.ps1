c05abdbe81353aef96c9d68777100c8c0d718719
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-16 15:56:04 +0200

    Add ability for limits

---
+++ Public/New-DashboardLimit.ps1
@@ -0,0 +1,20 @@
+﻿function New-DashboardLimit {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][string] $Name,
+        [nullable[int]] $LimitItem,
+        [nullable[DateTime]] $LimitDate
+    )
+
+    $Limit = [ordered] @{
+        Type     = 'FolderLimit'
+        Settings = [ordered] @{
+            Name      = $Name
+            LimitItem = $LimitItem
+            LimitDate = $LimitDate
+        }
+    }
+
+    Remove-EmptyValue -Hashtable $Limit.Settings
+    $Limit
+}
