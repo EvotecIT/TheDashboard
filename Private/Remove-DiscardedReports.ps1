ca17058f4eb369df4cc0a0e6b3319e13d61916b1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-28 13:38:03 +0200

    Add Remove-DiscardedReports function to project
    This commit adds a new file Remove-DiscardedReports.ps1 containing the Remove-DiscardedReports function. This function takes an array of file paths, a folder path, and an extension as input parameters, and removes any files in the folder that are not in the input array.

---
+++ Private/Remove-DiscardedReports.ps1
@@ -0,0 +1,17 @@
+﻿function Remove-DiscardedReports {
+    [CmdletBinding()]
+    param(
+        [Array] $FilePathsGenerated,
+        [string] $FolderPath,
+        [string] $Extension
+    )
+
+    #$FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*$($Extension)" | Select-Object -ExpandProperty FullName
+    $FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*" | Select-Object -ExpandProperty FullName
+    foreach ($File in $FilePathsCurrent) {
+        if ($File -notin $FilePathsGenerated) {
+            Write-Color -Text '[i]', '[FILE ] ', "Removing discarded report: $File" -Color Yellow, DarkGray, Yellow
+            Remove-Item -LiteralPath $File -Force
+        }
+    }
+}
