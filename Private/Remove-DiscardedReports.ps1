831ca241c87ce142f6deda90851ef5a36b91aaed
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-16 22:03:49 +0100

    Respect extension on delete

--- Private/Remove-DiscardedReports.ps1
+++ Private/Remove-DiscardedReports.ps1
@@ -7,7 +7,7 @@
     )
 
     #$FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*$($Extension)" | Select-Object -ExpandProperty FullName
-    $FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*" | Select-Object -ExpandProperty FullName
+    $FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*$($Extension)" | Select-Object -ExpandProperty FullName
     foreach ($File in $FilePathsCurrent) {
         if ($File -notin $FilePathsGenerated) {
             Write-Color -Text '[i]', '[FILE ] ', "Removing discarded report: $File" -Color Yellow, DarkGray, Yellow
