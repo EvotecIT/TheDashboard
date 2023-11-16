da62f02edabb21ddd5358b7e987e9b7707631474
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-16 22:53:27 +0100

    Fix sorting of hashtables

--- Private/Convert-FilesToMenu.ps1
+++ Private/Convert-FilesToMenu.ps1
@@ -13,7 +13,7 @@
         }
     }
     # We now build menu from files
-    foreach ($Entry in $Files | Sort-Object -Property Date -Descending) {
+    foreach ($Entry in $Files | Sort-Object { $_.Date } -Descending) {
         $LimitsConfiguration = $Folders[$Entry.Menu].LimitsConfiguration
         if ($LimitsConfiguration) {
             $Limits = $LimitsConfiguration[$Entry.Name]
