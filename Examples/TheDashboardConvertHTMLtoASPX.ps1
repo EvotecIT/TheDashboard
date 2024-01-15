3b727f9c2b6d225ab4e5b700031ad4aae6e5be2a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-15 11:36:51 +0100

    Add code to convert HTML to ASPX

---
+++ Examples/TheDashboardConvertHTMLtoASPX.ps1
@@ -0,0 +1,14 @@
+﻿$ExtensionFrom = '.html'
+$ExtensionTo = '.aspx'
+
+#$ExtensionFrom = '.aspx'
+#$ExtensionTo = '.html'
+
+$Files = Get-ChildItem -Path $PSScriptRoot\Reports -File -Recurse -Include "*$ExtensionFrom"
+foreach ($File in $Files) {
+    if ($File.Extension -eq $ExtensionFrom) {
+        # Rename extension to .aspx
+        $NewName = $File.FullName -replace "$($ExtensionFrom)$", $ExtensionTo
+        Rename-Item -Path $File.FullName -NewName $NewName -Force
+    }
+}
