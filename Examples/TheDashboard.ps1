416049c5119e0d6568c40741f1051240ce868bd2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-15 12:29:15 +0100

    Update example

--- Examples/TheDashboard.ps1
+++ Examples/TheDashboard.ps1
@@ -13,32 +13,32 @@ Start-TheDashboard -HTMLPath "$PSScriptRoot\Reports\Index.html" -Logo 'https://e
     New-DashboardGage -Label 'Computers' -MinValue 0 -MaxValue 200 -Value $AllComputers.Count -Date $Today
     New-DashboardGage -Label 'Groups' -MinValue 0 -MaxValue 200 -Value $AllGroups.Count -Date $Today
     New-DashboardGage -Label 'Group Policies' -MinValue 0 -MaxValue 200 -Value $AllGroupPolicies.Count -Date $Today
-    New-DashboardFolder -Name 'ActiveDirectory' -IconBrands gofore -UrlName 'ActiveDirectory' -Path $PSScriptRoot\Reports\ActiveDirectory {
-        New-DashboardReplacement -SplitOn "_" -AddSpaceToName
-        New-DashboardReplacement -BeforeSplit @{
-            'GPOZaurr'         = ''
-            'PingCastle-'      = ''
-            'Testimo'          = ''
-            'GroupMembership-' = ''
-            '_Regional'        = ' Regional'
-        }
-        New-DashboardReplacement -AfterSplit @{
-            'G P O'     = 'GPO'
-            'L A P S'   = 'LAPS'
-            'L D A P'   = 'LDAP'
-            'K R B G T' = 'KRBGT'
-            'I N S'     = 'INS'
-            'I T R X X' = 'ITRXX'
-            'A D'       = 'AD'
-            'D H C P'   = 'DHCP'
-        }
-        New-DashboardLimit -LimitItem 1 -IncludeHistory
-        #New-DashboardLimit -Name 'GPO Blocked Inheritance' -LimitItem 1
-        #New-DashboardLimit -Name 'Password Quality 1' -LimitItem 1
-    } -DisableGlobalReplacement
-    New-DashboardFolder -Name 'GroupPolicies' -IconBrands android -UrlName 'GPO' -Path $PSScriptRoot\Reports\GroupPolicies
-
-    New-DashboardReplacement -SplitOn "_" -AddSpaceToName
+    # New-DashboardFolder -Name 'Active Directory' -IconBrands gofore -UrlName 'ActiveDirectory' -Path $PSScriptRoot\Reports\ActiveDirectory {
+    #     New-DashboardReplacement -SplitOn "[_-]" -AddSpaceToName
+    #     New-DashboardReplacement -BeforeSplit @{
+    #         'GPOZaurr'         = ''
+    #         'PingCastle-'      = ''
+    #         'Testimo'          = ''
+    #         'GroupMembership-' = ''
+    #         '_Regional'        = ' Regional'
+    #     }
+    #     New-DashboardReplacement -AfterSplit @{
+    #         'G P O'     = 'GPO'
+    #         'L A P S'   = 'LAPS'
+    #         'L D A P'   = 'LDAP'
+    #         'K R B G T' = 'KRBGT'
+    #         'I N S'     = 'INS'
+    #         'I T R X X' = 'ITRXX'
+    #         'A D'       = 'AD'
+    #         'D H C P'   = 'DHCP'
+    #     }
+    #
+    #     #New-DashboardLimit -Name 'GPO Blocked Inheritance' -LimitItem 1
+    #     #New-DashboardLimit -Name 'Password Quality 1' -LimitItem 1
+    # } -DisableGlobalReplacement
+    New-DashboardFolder -Name 'Group Policies' -IconBrands android -UrlName 'GPO' -Path $PSScriptRoot\Reports\TestPolicies
+    #New-DashboardFolder -Name 'Test' -IconBrands android -UrlName 'New1' -Path $PSScriptRoot\Reports\New1
+    New-DashboardReplacement -SplitOn "[_-]" -AddSpaceToName
     New-DashboardReplacement -BeforeSplit @{ 'GPOZaurr' = '' }
     New-DashboardReplacement -BeforeSplit @{ 'GroupMembership-' = ''; '_Regional' = ' Regional' }
     New-DashboardReplacement -BeforeSplit @{
@@ -46,7 +46,7 @@ Start-TheDashboard -HTMLPath "$PSScriptRoot\Reports\Index.html" -Logo 'https://e
         'PingCastle-' = ''
         'Testimo'     = ''
     }
-
+    New-DashboardLimit -LimitItem 2 -IncludeHistory
     New-DashboardReplacement -AfterSplit @{ 'G P O' = 'GPO' }, @{  'L D A P' = 'LDAP' }, @{ 'D H C P' = 'DHCP' }
     New-DashboardReplacement -AfterSplit @{ 'L A P S' = 'LAPS' }, @{  'K R B G T' = 'KRBGT' }
     New-DashboardReplacement -AfterSplit @{ 'A D' = 'AD' }, @{  'I T R X X' = 'ITRXX' }, @{  'I N S' = 'INS' }
