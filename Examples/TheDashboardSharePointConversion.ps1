1fdf6a45e72d77e7fe56adad3d29b858057aef49
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-26 18:04:11 +0200

    Replace files

---
+++ Examples/TheDashboardSharePointConversion.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\TheDashboard.psd1 -Force
+
+Repair-DashboardExtension -Path $PSScriptRoot\Reports -ExtensionFrom ".html" -ExtensionTo ".aspx" -WhatIf
+Repair-DashboardExtension -Path $PSScriptRoot\Reports -ExtensionFrom ".aspx" -ExtensionTo ".html" -WhatIf
