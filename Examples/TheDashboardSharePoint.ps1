b04b321fd555064d553572ff2a555565a0ee5826
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-01 12:50:27 +0100

    Refactor TheDashboardSharePoint.ps1 to use HTML file and improve user/computer/group counts

--- Examples/TheDashboardSharePoint.ps1
+++ Examples/TheDashboardSharePoint.ps1
@@ -4,18 +4,24 @@ Import-Module .\TheDashboard.psd1 -Force
 # This is configuration that should be used for SharePoint
 # Please notice HTMLPath which has ASPX extension, as this is the only one supported by SharePoint
 # Please notice UrlPath which is absolute path to SharePoint site. This is required if you want to use SharePoint with ASPX file as HomePage
-Start-TheDashboard -HTMLPath "$PSScriptRoot\Reports\Index.aspx" -UrlPath "https://evotecpoland.sharepoint.com/sites/TheDashboard/SitePages" -Logo 'https://evotec.xyz/wp-content/uploads/2021/04/Logo-evotec-bb.png' -ShowHTML {
+Start-TheDashboard -HTMLPath "$PSScriptRoot\Reports\Index.html" -UrlPath "https://evotecpoland.sharepoint.com/sites/TheDashboard/SitePages" -Logo 'https://evotec.xyz/wp-content/uploads/2021/04/Logo-evotec-bb.png' -ShowHTML:$true {
     $Today = Get-Date
     $Forest = Get-ADForest
-    $AllUsers = $Forest.Domains | ForEach-Object { Get-ADUser -Filter * -Properties 'DistinguishedName' -Server $_ }
-    $AllComputers = $Forest.Domains | ForEach-Object { Get-ADComputer -Filter * -Properties 'DistinguishedName' -Server $_ }
-    $AllGroups = $Forest.Domains | ForEach-Object { Get-ADGroup -Filter * -Server $_ }
-    $AllGroupPolicies = $Forest.Domains | ForEach-Object { Get-GPO -All -Domain $_ }
+    # $AllUsersCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=user)" -Server $_).Count }
+    # $AllComputersCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=computer)" -Server $_).Count }
+    # $AllGroupsCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=group)" -Server $_).Count }
+    # $AllGroupPoliciesCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=groupPolicyContainer)" -Server $_).Count }
 
-    New-DashboardGage -Label 'Users' -MinValue 0 -MaxValue 500 -Value $AllUsers.Count -Date $Today
-    New-DashboardGage -Label 'Computers' -MinValue 0 -MaxValue 200 -Value $AllComputers.Count -Date $Today
-    New-DashboardGage -Label 'Groups' -MinValue 0 -MaxValue 200 -Value $AllGroups.Count -Date $Today
-    New-DashboardGage -Label 'Group Policies' -MinValue 0 -MaxValue 200 -Value $AllGroupPolicies.Count -Date $Today
+
+    $AllUsersCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=user)" -Server $_).Count } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $AllComputersCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=computer)" -Server $_).Count } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $AllGroupsCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=group)" -Server $_).Count } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $AllGroupPoliciesCount = $Forest.Domains | ForEach-Object { (Get-ADObject -LDAPFilter "(objectClass=groupPolicyContainer)" -Server $_).Count } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+
+    New-DashboardGage -Label 'Users' -MinValue 0 -MaxValue 500 -Value $AllUsersCount -Date $Today
+    New-DashboardGage -Label 'Computers' -MinValue 0 -MaxValue 200 -Value $AllComputersCount -Date $Today
+    New-DashboardGage -Label 'Groups' -MinValue 0 -MaxValue 200 -Value $AllGroupsCount -Date $Today
+    New-DashboardGage -Label 'Group Policies' -MinValue 0 -MaxValue 200 -Value $AllGroupPoliciesCount -Date $Today
     # New-DashboardFolder -Name 'Active Directory' -IconBrands gofore -UrlName 'ActiveDirectory' -Path $PSScriptRoot\Reports\ActiveDirectory {
     #     New-DashboardReplacement -SplitOn "[_-]" -AddSpaceToName
     #     New-DashboardReplacement -BeforeSplit @{
@@ -40,6 +46,39 @@ Start-TheDashboard -HTMLPath "$PSScriptRoot\Reports\Index.aspx" -UrlPath "https:
     #     #New-DashboardLimit -Name 'Password Quality 1' -LimitItem 1
     # } -DisableGlobalReplacement
     New-DashboardFolder -Name 'Group Policies' -IconBrands android -UrlName 'GPO' -Path $PSScriptRoot\Reports\TestPolicies
+    New-DashboardFolder -Name 'Windows Updates' -IconBrands android -UrlName 'WindowsUpdates' -Path $PSScriptRoot\Reports\WindowsUpdates {
+        $newDashboardReplacementSplat = @{
+            BeforeSplit             = @{
+                'GroupMembership-' = '';
+                '_Regional'        = ' Regional'
+                'GPOZaurr'         = ''
+                'PingCastle-'      = ''
+                'Testimo'          = ''
+            }
+            #AfterSplitPositionName  = 1, 2
+            AfterSplitPositionName  = [ordered] @{
+                'WindowsUpdates*' = 1, 2
+                '*'               = 1
+            }
+            AfterRemoveDoubleSpaces = $true
+            AfterUpperChars         = "evotec.com", "test.com"
+            AfterSplit              = @{
+                'G P O'     = 'GPO' ;
+                'L D A P'   = 'LDAP' ;
+                'D H C P'   = 'DHCP'
+                'A D'       = 'AD';
+                'I T R X X' = 'ITRXX' ;
+                'I N S'     = 'INS'
+                'L A P S'   = 'LAPS' ;
+                'K R B G T' = 'KRBGT'
+            }
+            SplitOn                 = "[_-]"
+            AddSpaceToName          = $true
+        }
+        New-DashboardReplacement @newDashboardReplacementSplat
+        New-DashboardLimit -LimitItem 2 -IncludeHistory
+    } -DisableGlobalReplacement
+
     #New-DashboardFolder -Name 'Test' -IconBrands android -UrlName 'New1' -Path $PSScriptRoot\Reports\New1
     New-DashboardReplacement -SplitOn "[_-]" -AddSpaceToName
     New-DashboardReplacement -BeforeSplit @{ 'GPOZaurr' = '' }
