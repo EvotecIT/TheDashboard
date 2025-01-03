7f2b3d1954d5626a00d04d9e390d4d5754de7838
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-03 18:13:49 +0100

    Add Limit-FilesHistory function to manage history limits in menu entries

---
+++ Private/Limit-FilesHistory.ps1
@@ -0,0 +1,26 @@
+﻿function Limit-FilesHistory {
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $MenuBuilder,
+        [PSCustomObject] $Entry,
+        [System.Collections.IDictionary] $Limits,
+        [DateTime] $CurrentDate
+    )
+    if ($Limits.IncludeHistory) {
+        if ($Limits.IncludeHistoryLimit) {
+            if ($MenuBuilder[$Entry.Menu][$Entry.Name]['History'].Count -ge $Limits.IncludeHistoryLimit) {
+                return
+            }
+        } elseif ($Limits.IncludeHistoryLimitDate) {
+            if ($Entry.Date -lt $Limits.IncludeHistoryLimitDate) {
+                return
+            }
+        } elseif ($Limits.IncludeHistoryLimitDays) {
+            if ($Entry.Date -lt ($CurrentDate).AddDays(-$Limits.IncludeHistoryLimitDays)) {
+                return
+            }
+        }
+        $Entry.Include = $true
+        $MenuBuilder[$Entry.Menu][$Entry.Name]['History'].Add($Entry)
+    }
+}
