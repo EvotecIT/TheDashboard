06c5fbde120aff85def2c99a964761927162ab03
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-03 18:13:55 +0100

    Add additional parameters to New-DashboardLimit for enhanced history management

--- Public/New-DashboardLimit.ps1
+++ Public/New-DashboardLimit.ps1
@@ -4,19 +4,32 @@
         [string] $Name,
         [nullable[int]] $LimitItem,
         [nullable[DateTime]] $LimitDate,
-        [switch] $IncludeHistory
+        [nullable[int]] $LimitDays,
+        [switch] $IncludeHistory,
+        [nullable[DateTime]] $IncludeHistoryLimitDate,
+        [nullable[int]] $IncludeHistoryLimitDays,
+        [nullable[int]] $IncludeHistoryLimit
     )
 
     if (-not $Name) {
         $Name = '*'
     }
+
+    if ($IncludeHistoryLimitDate -or $IncludeHistoryLimitDays -or $IncludeHistoryLimit) {
+        $IncludeHistory = $true
+    }
+
     $Limit = [ordered] @{
         Type     = 'FolderLimit'
         Settings = [ordered] @{
-            Name           = $Name
-            LimitItem      = $LimitItem
-            LimitDate      = $LimitDate
-            IncludeHistory = $IncludeHistory
+            Name                    = $Name
+            LimitItem               = $LimitItem
+            LimitDate               = $LimitDate
+            LimitDays               = $LimitDays
+            IncludeHistory          = $IncludeHistory
+            IncludeHistoryLimit     = $IncludeHistoryLimit
+            IncludeHistoryLimitDate = $IncludeHistoryLimitDate
+            IncludeHistoryLimitDays = $IncludeHistoryLimitDays
         }
     }
 
