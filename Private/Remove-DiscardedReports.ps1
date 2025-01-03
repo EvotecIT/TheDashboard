05fa57295930b9934340d7a1371c52f1a4f1ab5c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-03 16:26:33 +0100

    Remove commented code from Remove-DiscardedReports.ps1 for clarity

--- Private/Remove-DiscardedReports.ps1
+++ Private/Remove-DiscardedReports.ps1
@@ -5,8 +5,6 @@
         [string] $FolderPath,
         [string] $Extension
     )
-
-    #$FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*$($Extension)" | Select-Object -ExpandProperty FullName
     $FilePathsCurrent = Get-ChildItem -LiteralPath $FolderPath -File -Filter "*$($Extension)" | Select-Object -ExpandProperty FullName
     foreach ($File in $FilePathsCurrent) {
         if ($File -notin $FilePathsGenerated) {
