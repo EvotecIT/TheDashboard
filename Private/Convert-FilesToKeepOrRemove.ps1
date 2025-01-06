2411e339a851ad1b0c43bd49213295d0488b1d61
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-06 08:47:17 +0100

    Add Convert-FilesToKeepOrRemove function for file management
    - Introduced a new function to manage file inclusion/exclusion.
    - Supports multiple delete methods: RemoveItem, DotNetDelete, and RecycleBin.
    - Implements optional removal of files not included in the summary.

---
+++ Private/Convert-FilesToKeepOrRemove.ps1
@@ -0,0 +1,44 @@
+﻿function Convert-FilesToKeepOrRemove {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Array] $FilePathsGenerated,
+        [Array] $Files,
+        [switch] $RemoveNotIncluded,
+        [ValidateSet("RemoveItem", "DotNetDelete", "RecycleBin")]
+        [string] $DeleteMethod = "RecycleBin"
+    )
+    [Array] $FullSummary = @(
+        foreach ($File in $FilePathsGenerated) {
+            [PSCustomObject] @{
+                Type    = 'Dashboard'
+                Path    = $File
+                Include = $true
+                Date    = (Get-Item -LiteralPath $File).LastWriteTime
+                Status  = if ($Include) { 'Included' } else { 'Excluded' }
+            }
+
+            foreach ($File in $Files) {
+                [PSCustomObject] @{
+                    Type    = 'Report'
+                    Path    = $File.FullPath
+                    Include = $File.Include
+                    Date    = $File.Date
+                    Status  = if ($File.Include) { 'Included' } else { 'Excluded' }
+                }
+            }
+        }
+    )
+    if ($RemoveNotIncluded) {
+        foreach ($File in $FullSummary) {
+            if (-not $File.Include) {
+                Remove-FileItem -Paths $File.Path -DeleteMethod $DeleteMethod -WhatIf:$WhatIfPreference
+                if ($WhatIfPreference) {
+                    $File.Status = 'Removed (WhatIf)'
+                } else {
+                    $File.Status = 'Removed'
+                }
+            }
+        }
+    }
+    $FullSummary
+}
