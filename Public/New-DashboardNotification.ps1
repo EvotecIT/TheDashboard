b61d128a862e08fb93a2e6db395dfc8705064f75
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-21 11:23:21 +0100

    Add New-DashboardNotification function for creating notifications
    - Introduced New-DashboardNotification function
    - Supports parameters for Name, Category, Type, Filter, EmailTo, EmailCC, and EmailSubject
    - Constructs a notification object with specified settings

---
+++ Public/New-DashboardNotification.ps1
@@ -0,0 +1,28 @@
+﻿function New-DashboardNotification {
+    [CmdletBinding(DefaultParameterSetName = 'Report')]
+    param(
+        [Parameter(ParameterSetName = 'Report')][string] $Name,
+        [Parameter(ParameterSetName = 'Report')][string] $Category,
+        [string] $Type,
+        [string] $Filter,
+        [Parameter(Mandatory)][string] $EmailTo,
+        [Parameter()][string] $EmailCC,
+        [Parameter(Mandatory)][string] $EmailSubject
+    )
+
+    $Notification = [ordered] @{
+        Type     = 'Notification'
+        Settings = [ordered] @{
+            Id           = [Guid]::NewGuid().ToString()
+            Name         = $Name
+            Category     = $Category
+            Type         = $Type
+            Filter       = $Filter
+            EmailTo      = $EmailTo
+            EmailCC      = $EmailCC
+            EmailSubject = $EmailSubject
+        }
+    }
+    Remove-EmptyValue -Hashtable $Notification
+    $Notification
+}
