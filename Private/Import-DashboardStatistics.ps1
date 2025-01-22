9b1b4b661715899c6fc9ee61de825998d9a314f5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-22 12:21:26 +0100

    Add Import-DashboardStatistics function - Introduced a new function to import dashboard statistics from a specified path. - Validates the path and imports data using Import-Clixml. - Initializes an ordered hashtable to store statistics by date.

---
+++ Private/Import-DashboardStatistics.ps1
@@ -0,0 +1,16 @@
+﻿function Import-DashboardStatistics {
+    [CmdletBinding()]
+    param(
+        [string] $StatisticsPath
+    )
+    $TopStats = [ordered] @{}
+    if ($StatisticsPath -and (Test-Path -LiteralPath $StatisticsPath)) {
+        Write-Color -Text '[i]', "[TheDashboard] ", 'Importing Statistics', ' [Informative] ', $StatisticsPath -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
+        $TopStats = Import-Clixml -LiteralPath $StatisticsPath
+    }
+    foreach ($E in $GageConfiguration) {
+        $TopStats[$E.Date] = [ordered] @{}
+        $TopStats[$E.Date].Date = $E.Date
+    }
+    $TopStats
+}
