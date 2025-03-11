969b11492d14413510c6da3557039d999e82a162
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-11 08:47:50 +0100

    Enhance Copy-HTMLFiles.ps1 to support multiple file extensions
    - Added functionality to copy and move files based on multiple extensions.
    - Improved directory creation logic for destination paths.

--- Private/Copy-HTMLFiles.ps1
+++ Private/Copy-HTMLFiles.ps1
@@ -15,14 +15,31 @@
                     $null = New-Item -Path $DIrectoryName -ItemType Directory -Force
                     Copy-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
                 }
+                foreach ($Ext in $Folders[$FolderName].Extension) {
+                    foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Ext" -Recurse) {
+                        $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
+                        $DIrectoryName = [io.path]::GetDirectoryName($Destination)
+                        $null = New-Item -Path $DIrectoryName -ItemType Directory -Force
+                        Copy-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
+                    }
+                }
             }
-        } elseif ($Folders[$FolderName].MoveFrom) {
+        }
+        if ($Folders[$FolderName].MoveFrom) {
             foreach ($Path in $Folders[$FolderName].MoveFrom) {
                 foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Extension" -Recurse) {
                     $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
                     $DIrectoryName = [io.path]::GetDirectoryName($Destination)
                     $null = New-Item -Path $DIrectoryName -ItemType Directory -Force
                     Move-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
+                }
+                foreach ($Ext in $Folders[$FolderName].Extension) {
+                    foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Ext" -Recurse) {
+                        $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
+                        $DIrectoryName = [io.path]::GetDirectoryName($Destination)
+                        $null = New-Item -Path $DIrectoryName -ItemType Directory -Force
+                        Move-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
+                    }
                 }
             }
         }
