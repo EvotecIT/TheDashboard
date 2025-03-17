f8c5ac51e2b6b4618f9725bc9230635b19276473
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-17 08:27:57 +0100

    Enhance Copy-HTMLFiles.ps1 with improved logging
    - Updated log messages to specify whether files are being copied or moved
    - Added detailed logging for source and destination paths for both copy and move operations

--- Private/Copy-HTMLFiles.ps1
+++ Private/Copy-HTMLFiles.ps1
@@ -5,10 +5,11 @@
         [string] $Extension
     )
 
-    Write-Color -Text '[i]', "[TheDashboard] ", 'Copying or HTML files', ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
+    Write-Color -Text '[i]', "[TheDashboard] ", 'Copying or moving HTML files', ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
     foreach ($FolderName in $Folders.Keys) {
         if ($Folders[$FolderName].CopyFrom) {
             foreach ($Path in $Folders[$FolderName].CopyFrom) {
+                Write-Color -Text '[i]', "[TheDashboard] ", "Copying from $Path to $($Folders[$FolderName].Path) using '$Extension'", ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
                 foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Extension" -Recurse) {
                     $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
                     $DIrectoryName = [io.path]::GetDirectoryName($Destination)
@@ -16,6 +17,7 @@
                     Copy-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
                 }
                 foreach ($Ext in $Folders[$FolderName].Extension) {
+                    Write-Color -Text '[i]', "[TheDashboard] ", "Copying from $Path to $($Folders[$FolderName].Path) using '$Ext'", ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
                     foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Ext" -Recurse) {
                         $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
                         $DIrectoryName = [io.path]::GetDirectoryName($Destination)
@@ -27,6 +29,7 @@
         }
         if ($Folders[$FolderName].MoveFrom) {
             foreach ($Path in $Folders[$FolderName].MoveFrom) {
+                Write-Color -Text '[i]', "[TheDashboard] ", "Moving from $Path to $($Folders[$FolderName].Path) using '$Extension'", ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
                 foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Extension" -Recurse) {
                     $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
                     $DIrectoryName = [io.path]::GetDirectoryName($Destination)
@@ -34,6 +37,7 @@
                     Move-Item -LiteralPath $File.FullName -Destination $Destination -Force -ErrorAction Stop
                 }
                 foreach ($Ext in $Folders[$FolderName].Extension) {
+                    Write-Color -Text '[i]', "[TheDashboard] ", "Moving from $Path to $($Folders[$FolderName].Path) using '$Ext'", ' [Informative] ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
                     foreach ($File in Get-ChildItem -LiteralPath $Path -Filter "*$Ext" -Recurse) {
                         $Destination = $File.FullName.Replace($Path, $Folders[$FolderName].Path)
                         $DIrectoryName = [io.path]::GetDirectoryName($Destination)
