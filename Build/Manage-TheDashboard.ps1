bad3b67bd5853e870ef70152e2e20e9577264635
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-11 11:23:34 +0200

    Refactor Manage-TheDashboard.ps1 to streamline module configuration
    - Removed commented-out code for ignored modules and functions to clean up the script.
    - Ensured all functions are available by importing the module self.
    - Enabled publishing to PowerShell Gallery and GitHub with the appropriate configurations.

--- Build/Manage-TheDashboard.ps1
+++ Build/Manage-TheDashboard.ps1
@@ -35,15 +35,6 @@ Invoke-ModuleBuild -ModuleName 'TheDashboard' {
     )
     New-ConfigurationModule -Type ApprovedModule -Name 'PSSharedGoods', 'PSWriteColor', 'Connectimo', 'PSUnifi', 'PSWebToolbox', 'PSMyPassword'
 
-    # New-ConfigurationModuleSkip -IgnoreModuleName @(
-    #     # this are builtin into PowerShell, so not critical
-    #     'Microsoft.PowerShell.Management'
-    #     'Microsoft.PowerShell.Security'
-    #     'Microsoft.PowerShell.Utility'
-    #     # this is optional, and checked for existance in the source codes directly
-    #     'PSParseHTML'
-    # ) -IgnoreFunctionName 'Select-Unique', 'Compare-TwoArrays' # those functions are internal within private function
-
     $ConfigurationFormat = [ordered] @{
         RemoveComments                              = $true
         RemoveEmptyLines                            = $true
@@ -86,17 +77,15 @@ Invoke-ModuleBuild -ModuleName 'TheDashboard' {
     New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'OnMergePSD1' -PSD1Style 'Minimal'
     # configuration for documentation, at the same time it enables documentation processing
     New-ConfigurationDocumentation -Enable:$false -StartClean -UpdateWhenNew -PathReadme 'Docs\Readme.md' -Path 'Docs'
-
+    # import self to make sure that all functions are available
     New-ConfigurationImportModule -ImportSelf
-
+    # build the module
     New-ConfigurationBuild -Enable:$true -SignModule -MergeModuleOnBuild -MergeFunctionsFromApprovedModules -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
-
-    #New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
-
+    # create artefacts
     New-ConfigurationArtefact -Type Unpacked -Enable -Path "$PSScriptRoot\..\Artefacts\Unpacked" -AddRequiredModules
     New-ConfigurationArtefact -Type Packed -Enable -Path "$PSScriptRoot\..\Artefacts\Packed" -ArtefactName '<ModuleName>.v<ModuleVersion>.zip'
 
     # options for publishing to github/psgallery
-    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 } -ExitCode -IncludeAll @()
